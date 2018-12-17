14cb531a75de6d35bde0b4b10f19b1d1c9cdb85d
abrockmeier <alexander.brockmeier@ambicotech.com>
2018-12-17 09:42:49 +0100

    v0.3b
    nicht lauffähige Version!...

--- ZahlenSort.cs
+++ ZahlenSort.cs
@@ -10,7 +10,7 @@ namespace nZahlenSortieren
 		 * static void DisplayArray(string[] arr) => Console.WriteLine(string.Join(" ", arr));
 		 */
 		static void DisplayArray(Int32[] arr) => Console.WriteLine(string.Join(" ", arr));
-		
+
 		static void Main(string[] args)
 		{
 
@@ -22,15 +22,14 @@ namespace nZahlenSortieren
 			bool korrekteEingabe;
 			bool korrekteEingabeVergleichJ;
 			int anzahlElemente = 0;
-			//  string zwischenSpeicher;			-->UNSUSED!
-			//  bool endeDerEingabe = false;		-->UNSUSED!
 			int[] eingabeArray;
-			int i = 0;						  //-->NAMENSKONVENTIONEN...
-			int x;							  //--> s.o.
-			int xN;							  //--> s.o.
-			
+			int i = 0;                        //-->NAMENSKONVENTIONEN...
+			int x;                            //--> s.o.
+			int xN;
+			int arrayMin = 0; //--> s.o.
+
 			// --> Eingabe (Abfrage der Größe des Arrays und der Elemente)
-			
+
 			while (anzahlElemente <= 0)
 			{
 				Console.WriteLine("Wie viele Zahlen möchten Sie eingeben:");
@@ -55,14 +54,14 @@ namespace nZahlenSortieren
 				Console.WriteLine("Na fein... Sie haben die Anzahl von {0} Zahlen gewählt", eingabe);
 				i = Convert.ToInt32(eingabe);
 			}
-		eingabeArray = new int[anzahlElemente];
-		korrekteEingabe = true;
-		for (x = 0; x < i; x++)
-		{
-			xN = x + 1;
-			Console.WriteLine("Bitte {0}. Zahl eingeben:", xN);
-			eingabe = Console.ReadLine();
-			korrekteEingabe = int.TryParse(Convert.ToString(eingabe), out eingabeIsInt);
+			eingabeArray = new int[anzahlElemente];
+			korrekteEingabe = true;
+			for (x = 0; x < i; x++)
+			{
+				xN = x + 1;
+				Console.WriteLine("Bitte {0}. Zahl eingeben:", xN);
+				eingabe = Console.ReadLine();
+				korrekteEingabe = int.TryParse(Convert.ToString(eingabe), out eingabeIsInt);
 				while (korrekteEingabe == false)
 				{
 					Console.WriteLine("Bitte {0}. Zahl eingeben:", xN);
@@ -70,40 +69,80 @@ namespace nZahlenSortieren
 					eingabe = Console.ReadLine();
 					korrekteEingabe = int.TryParse(Convert.ToString(eingabe), out eingabeIsInt);
 				}
-			eingabeArray[x] = Convert.ToInt32(eingabe);
+				eingabeArray[x] = Convert.ToInt32(eingabe);
 
-			// --> TODO: Fehlerhafte eingabe von Zahlen Abfangen... )
-			
-		}
+				// --> TODO: Fehlerhafte eingabe von Zahlen Abfangen... )
 
-		// -->  Ausgabe (Teil Eingegebene Zahlen)
+			}
 
-		Console.WriteLine("Die Eingegebenen Zahlen lauten:");
-		DisplayArray(eingabeArray);
-		
-		Console.WriteLine("Um den Eingegebenen Array vor dem Beenden aufsteigend " +
-							"zu sortieren drücken sie bitte \"j\"!");
-		Console.WriteLine("Alle anderen eingaben führen zum Beenden des " +
-							"Programms!!! ;-) Vermeintlich... ");
-		eingabe = Console.ReadLine();
-		korrekteEingabe = Int32.TryParse(eingabe, out einAusgabe);
-		korrekteEingabeVergleichJ = Int32.TryParse("j", out einAusgabeJ);
-		
-		// --> Verarbeitung
+			// -->  Ausgabe (Teil Eingegebene Zahlen)
 
-		if (korrekteEingabe == korrekteEingabeVergleichJ)
-		{
-			//TODO Sortierung selber implementieren... Gut´s Nächtle!	
-			Array.Sort(eingabeArray); 
-		
-			// -->  Ausgabe (Teil sortierte Zahlen)
+			Console.WriteLine("Die Eingegebenen Zahlen lauten:");
+			DisplayArray(eingabeArray);
 
-			// hier in diesem Fall zur Ausgabe verwendet, da "umsortiert" ;-) 
-			DisplayArray(eingabeArray);    
-			Console.WriteLine("fertig...");
-		}
-		else
-			Console.WriteLine("Vielen Dank und bis später...");
+			Console.WriteLine("Um den Eingegebenen Array vor dem Beenden aufsteigend " +
+								"zu sortieren drücken sie bitte \"s\"!");
+			Console.WriteLine("Um den Eingegebenen Array selbst vor dem Beenden aufsteigend " +
+								"zu sortieren drücken sie bitte \"t\"!");
+			Console.WriteLine("Alle anderen eingaben führen zum Beenden des " +
+								"Programms!!! ;-) Vermeintlich... ");
+			eingabe = Console.ReadLine();
+			korrekteEingabe = Int32.TryParse(eingabe, out einAusgabe);
+
+			// hier sollte geprüft werden ob die eingabe wirklich s oder t oder etwas anderes war! 
+			korrekteEingabeVergleichJ = Int32.TryParse("s", out einAusgabeJ);
+			// --> Verarbeitung
+			/*
+			if (korrekteEingabe == korrekteEingabeVergleichJ)
+			{
+				//TODO Sortierung selber implementieren... Gut´s Nächtle!	
+				Array.Sort(eingabeArray);
+
+				// -->  Ausgabe (Teil sortierte Zahlen)
+
+				// hier in diesem Fall zur Ausgabe verwendet, da "umsortiert" ;-) 
+				DisplayArray(eingabeArray);
+				Console.WriteLine("fertig...");
+			}
+			*/
+			//if (korrekteEingabe)
+
+			// Nochmal in Ruhe über die Schleifensteuerung nachdenken,...
+			{
+				for (int j = 0; j <= eingabeArray.Length; j++)
+				{
+					for (int k = 0; k < eingabeArray.Length; k++)
+					{
+						if (eingabeArray[k] > eingabeArray[j])
+						{
+							arrayMin = eingabeArray[k];
+							eingabeArray[k] = eingabeArray[j];
+							eingabeArray[j] = arrayMin;
+						}
+						
+					}
+					if ((eingabeArray[eingabeArray.Length -1] < eingabeArray[0]))
+					{
+						arrayMin = eingabeArray[0];
+						eingabeArray[0] = eingabeArray[eingabeArray.Length -1];
+						eingabeArray[eingabeArray.Length-1] = arrayMin;
+					}
+					//if (eingabeArray[j] <= arrayMin)
+					//{
+					//	arrayMin = eingabeArray[j];
+					//}
+				}
+				DisplayArray(eingabeArray);
+				Console.WriteLine("fertig...");
+
+				foreach (int elem in eingabeArray)
+				{
+					// Iterator für die augabe (manuelle Variante)
+
+				}
+			} 
+			//else
+				Console.WriteLine("Vielen Dank und bis später...");
 		Console.ReadKey();
 		}
 	}
@@ -112,7 +151,7 @@ namespace nZahlenSortieren
 
 	public class Author : System.Attribute  
 {  
-    private string name;  
+    private readonly string name;  
     public double version;  
   
     public Author(string name)  
